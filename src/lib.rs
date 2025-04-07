5b0d5b007a303d5de8b2e9a1de754276dfbd3c05
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-04-07 10:05:42 -0400

    moved in lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
 #![no_std]
-#![warn(clippy::wildcard_imports)]
-#![warn(clippy::needless_pass_by_ref_mut)]
-#![warn(clippy::undocumented_unsafe_blocks)]
-#![warn(missing_docs)]
 #![doc = include_str!("../README.md")]
 
 pub mod noise;
