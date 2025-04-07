cfaa65a398b793493a162b87675e67650eb30530
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-04-06 22:37:11 -0400

    organized project

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![no_std]
+#![warn(clippy::wildcard_imports)]
+#![warn(clippy::needless_pass_by_ref_mut)]
+#![warn(clippy::undocumented_unsafe_blocks)]
+#![warn(missing_docs)]
+#![doc = include_str!("../README.md")]
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-}
+mod tests {}
