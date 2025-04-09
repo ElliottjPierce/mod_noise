19354fd8be1148adb9a6d1759d7201b2564486b2
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-04-08 23:59:02 -0400

    3da and 4d

--- src/noise/grid.rs
+++ src/noise/grid.rs
@@ -366,6 +366,84 @@ impl GridSquare<UVec3, Vec3A> {
     }
 }
 
+impl SamplablePeriodicPoints for GridSquare<UVec3, Vec3A> {
+    #[inline]
+    fn sample_smooth<T, L: Curve<T>>(
+        &self,
+        f: impl FnMut(Self::Point) -> T,
+        lerp: impl Fn(T, T) -> L,
+        curve: impl Curve<f32>,
+    ) -> T {
+        // points
+        let [ldb, ldf, lub, luf, rdb, rdf, rub, ruf] = self.corners_map(f);
+        let mix = self.offset_from_corner.map(|t| curve.sample_unchecked(t));
+
+        // lerp
+        let ld = lerp(ldb, ldf).sample_unchecked(mix.z);
+        let lu = lerp(lub, luf).sample_unchecked(mix.z);
+        let rd = lerp(rdb, rdf).sample_unchecked(mix.z);
+        let ru = lerp(rub, ruf).sample_unchecked(mix.z);
+        let l = lerp(ld, lu).sample_unchecked(mix.y);
+        let r = lerp(rd, ru).sample_unchecked(mix.y);
+        lerp(l, r).sample_unchecked(mix.x)
+    }
+}
+
+impl DiferentiablePeriodicPoints for GridSquare<UVec3, Vec3A> {
+    type Gradient<D> = [D; 3];
+
+    #[inline]
+    fn sample_gradient_smooth<T: bevy_math::HasTangent, L: Curve<T::Tangent>>(
+        &self,
+        f: impl FnMut(Self::Point) -> T,
+        difference: impl Fn(&T, &T) -> T::Tangent,
+        lerp: impl Fn(T::Tangent, T::Tangent) -> L,
+        curve: impl bevy_math::curve::derivatives::SampleDerivative<f32>,
+    ) -> Self::Gradient<T::Tangent> {
+        // points// points
+        let [ldb, ldf, lub, luf, rdb, rdf, rub, ruf] = self.corners_map(f);
+        let [mix_x, mix_y, mix_z] = self
+            .offset_from_corner
+            .to_array()
+            .map(|t| curve.sample_with_derivative_unchecked(t));
+
+        // derivatives
+        let ldb_ldf = difference(&ldb, &ldf);
+        let lub_luf = difference(&lub, &luf);
+        let rdb_rdf = difference(&rdb, &rdf);
+        let rub_ruf = difference(&rub, &ruf);
+
+        let ldb_lub = difference(&ldb, &lub);
+        let ldf_luf = difference(&ldf, &luf);
+        let rdb_rub = difference(&rdb, &rub);
+        let rdf_ruf = difference(&rdf, &ruf);
+
+        let ldb_rdb = difference(&ldb, &rdb);
+        let ldf_rdf = difference(&ldf, &rdf);
+        let lub_rub = difference(&lub, &rub);
+        let luf_ruf = difference(&luf, &ruf);
+
+        // lerp
+        let dx = {
+            let d = lerp(ldb_rdb, ldf_rdf).sample_unchecked(mix_z.value);
+            let u = lerp(lub_rub, luf_ruf).sample_unchecked(mix_z.value);
+            lerp(d, u).sample_unchecked(mix_y.value)
+        } * mix_x.derivative;
+        let dy = {
+            let l = lerp(ldb_lub, ldf_luf).sample_unchecked(mix_z.value);
+            let r = lerp(rdb_rub, rdf_ruf).sample_unchecked(mix_z.value);
+            lerp(l, r).sample_unchecked(mix_x.value)
+        } * mix_y.derivative;
+        let dz = {
+            let l = lerp(ldb_ldf, lub_luf).sample_unchecked(mix_y.value);
+            let r = lerp(rdb_rdf, rub_ruf).sample_unchecked(mix_y.value);
+            lerp(l, r).sample_unchecked(mix_x.value)
+        } * mix_z.derivative;
+
+        [dx, dy, dz]
+    }
+}
+
 impl GridSquare<UVec4, Vec4> {
     #[inline]
     fn corners(&self) -> [OrthoGridLattacePoint<UVec4, Vec4>; 16] {
@@ -395,6 +473,167 @@ impl GridSquare<UVec4, Vec4> {
             f(self.from_offset(UVec4::new(1, 1, 1, 0))),
             f(self.from_offset(UVec4::new(1, 1, 1, 1))),
         ]
+    }
+}
+
+impl SamplablePeriodicPoints for GridSquare<UVec4, Vec4> {
+    #[inline]
+    fn sample_smooth<T, L: Curve<T>>(
+        &self,
+        f: impl FnMut(Self::Point) -> T,
+        lerp: impl Fn(T, T) -> L,
+        curve: impl Curve<f32>,
+    ) -> T {
+        // points
+        let [
+            ldbp,
+            ldbq,
+            ldfp,
+            ldfq,
+            lubp,
+            lubq,
+            lufp,
+            lufq,
+            rdbp,
+            rdbq,
+            rdfp,
+            rdfq,
+            rubp,
+            rubq,
+            rufp,
+            rufq,
+        ] = self.corners_map(f);
+        let mix = self.offset_from_corner.map(|t| curve.sample_unchecked(t));
+
+        // lerp
+        let ldb = lerp(ldbp, ldbq).sample_unchecked(mix.w);
+        let ldf = lerp(ldfp, ldfq).sample_unchecked(mix.w);
+        let lub = lerp(lubp, lubq).sample_unchecked(mix.w);
+        let luf = lerp(lufp, lufq).sample_unchecked(mix.w);
+        let rdb = lerp(rdbp, rdbq).sample_unchecked(mix.w);
+        let rdf = lerp(rdfp, rdfq).sample_unchecked(mix.w);
+        let rub = lerp(rubp, rubq).sample_unchecked(mix.w);
+        let ruf = lerp(rufp, rufq).sample_unchecked(mix.w);
+        let ld = lerp(ldb, ldf).sample_unchecked(mix.z);
+        let lu = lerp(lub, luf).sample_unchecked(mix.z);
+        let rd = lerp(rdb, rdf).sample_unchecked(mix.z);
+        let ru = lerp(rub, ruf).sample_unchecked(mix.z);
+        let l = lerp(ld, lu).sample_unchecked(mix.y);
+        let r = lerp(rd, ru).sample_unchecked(mix.y);
+        lerp(l, r).sample_unchecked(mix.x)
+    }
+}
+
+impl DiferentiablePeriodicPoints for GridSquare<UVec4, Vec4> {
+    type Gradient<D> = [D; 4];
+
+    #[inline]
+    fn sample_gradient_smooth<T: bevy_math::HasTangent, L: Curve<T::Tangent>>(
+        &self,
+        f: impl FnMut(Self::Point) -> T,
+        difference: impl Fn(&T, &T) -> T::Tangent,
+        lerp: impl Fn(T::Tangent, T::Tangent) -> L,
+        curve: impl bevy_math::curve::derivatives::SampleDerivative<f32>,
+    ) -> Self::Gradient<T::Tangent> {
+        // points// points
+        let [
+            ldbp,
+            ldbq,
+            ldfp,
+            ldfq,
+            lubp,
+            lubq,
+            lufp,
+            lufq,
+            rdbp,
+            rdbq,
+            rdfp,
+            rdfq,
+            rubp,
+            rubq,
+            rufp,
+            rufq,
+        ] = self.corners_map(f);
+        let [mix_x, mix_y, mix_z, mix_w] = self
+            .offset_from_corner
+            .to_array()
+            .map(|t| curve.sample_with_derivative_unchecked(t));
+
+        // derivatives
+        let ldbp_ldbq = difference(&ldbp, &ldbq);
+        let ldfp_ldfq = difference(&ldfp, &ldfq);
+        let lubp_lubq = difference(&lubp, &lubq);
+        let lufp_lufq = difference(&lufp, &lufq);
+        let rdbp_rdbq = difference(&rdbp, &rdbq);
+        let rdfp_rdfq = difference(&rdfp, &rdfq);
+        let rubp_rubq = difference(&rubp, &rubq);
+        let rufp_rufq = difference(&rufp, &rufq);
+
+        let ldbp_ldfp = difference(&ldbp, &ldfp);
+        let lubp_lufp = difference(&lubp, &lufp);
+        let rdbp_rdfp = difference(&rdbp, &rdfp);
+        let rubp_rufp = difference(&rubp, &rufp);
+        let ldbq_ldfq = difference(&ldbq, &ldfq);
+        let lubq_lufq = difference(&lubq, &lufq);
+        let rdbq_rdfq = difference(&rdbq, &rdfq);
+        let rubq_rufq = difference(&rubq, &rufq);
+
+        let ldbp_lubp = difference(&ldbp, &lubp);
+        let ldfp_lufp = difference(&ldfp, &lufp);
+        let rdbp_rubp = difference(&rdbp, &rubp);
+        let rdfp_rufp = difference(&rdfp, &rufp);
+        let ldbq_lubq = difference(&ldbq, &lubq);
+        let ldfq_lufq = difference(&ldfq, &lufq);
+        let rdbq_rubq = difference(&rdbq, &rubq);
+        let rdfq_rufq = difference(&rdfq, &rufq);
+
+        let ldbp_rdbp = difference(&ldbp, &rdbp);
+        let ldfp_rdfp = difference(&ldfp, &rdfp);
+        let lubp_rubp = difference(&lubp, &rubp);
+        let lufp_rufp = difference(&lufp, &rufp);
+        let ldbq_rdbq = difference(&ldbq, &rdbq);
+        let ldfq_rdfq = difference(&ldfq, &rdfq);
+        let lubq_rubq = difference(&lubq, &rubq);
+        let lufq_rufq = difference(&lufq, &rufq);
+
+        // lerp
+        let dx = {
+            let db = lerp(ldbp_rdbp, ldbq_rdbq).sample_unchecked(mix_w.value);
+            let df = lerp(ldfp_rdfp, ldfq_rdfq).sample_unchecked(mix_w.value);
+            let ub = lerp(lubp_rubp, lubq_rubq).sample_unchecked(mix_w.value);
+            let uf = lerp(lufp_rufp, lufq_rufq).sample_unchecked(mix_w.value);
+            let d = lerp(db, df).sample_unchecked(mix_z.value);
+            let u = lerp(ub, uf).sample_unchecked(mix_z.value);
+            lerp(d, u).sample_unchecked(mix_y.value)
+        } * mix_x.derivative;
+        let dy = {
+            let lb = lerp(ldbp_lubp, ldbq_lubq).sample_unchecked(mix_w.value);
+            let lf = lerp(ldfp_lufp, ldfq_lufq).sample_unchecked(mix_w.value);
+            let rb = lerp(rdbp_rubp, rdbq_rubq).sample_unchecked(mix_w.value);
+            let rf = lerp(rdfp_rufp, rdfq_rufq).sample_unchecked(mix_w.value);
+            let l = lerp(lb, lf).sample_unchecked(mix_z.value);
+            let r = lerp(rb, rf).sample_unchecked(mix_z.value);
+            lerp(l, r).sample_unchecked(mix_x.value)
+        } * mix_y.derivative;
+        let dz = {
+            let ld = lerp(ldbp_ldfp, ldbq_ldfq).sample_unchecked(mix_w.value);
+            let lu = lerp(lubp_lufp, lubq_lufq).sample_unchecked(mix_w.value);
+            let rd = lerp(rdbp_rdfp, rdbq_rdfq).sample_unchecked(mix_w.value);
+            let ru = lerp(rubp_rufp, rubq_rufq).sample_unchecked(mix_w.value);
+            let d = lerp(ld, rd).sample_unchecked(mix_x.value);
+            let u = lerp(lu, ru).sample_unchecked(mix_x.value);
+            lerp(d, u).sample_unchecked(mix_y.value)
+        } * mix_z.derivative;
+        let dw = {
+            let ld = lerp(ldbp_ldbq, ldfp_ldfq).sample_unchecked(mix_z.value);
+            let lu = lerp(lubp_lubq, lufp_lufq).sample_unchecked(mix_z.value);
+            let rd = lerp(rdbp_rdbq, rdfp_rdfq).sample_unchecked(mix_z.value);
+            let ru = lerp(rubp_rubq, rufp_rufq).sample_unchecked(mix_z.value);
+            let d = lerp(ld, rd).sample_unchecked(mix_x.value);
+            let u = lerp(lu, ru).sample_unchecked(mix_x.value);
+            lerp(d, u).sample_unchecked(mix_y.value)
+        } * mix_w.derivative;
+        [dx, dy, dz, dw]
     }
 }
 
